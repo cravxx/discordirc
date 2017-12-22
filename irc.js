adccffe2acb402ceeb141773521c6719bda1b5c9
Joelixny <joeliny_9@teknik.io>
2017-12-22 10:28:53 -0700

    added attachments and actions

--- irc.js
+++ irc.js
@@ -31,17 +31,33 @@ client.addListener('message', function (from, to, message) {
       message = message.substr(6,message.length);
       force = true;
     }
-    if(message === '!optout'){
+    if(message === '!optin'){
       if(optOuts.indexOf(from) === -1){
         optOuts.push(from);
         updateOptOuts(optOuts);
       }
-    } else if(message === '!optin'){
+    } else if(message === '!optout'){
       optOuts = optOuts.filter(i => i !== from);
       updateOptOuts(optOuts);
     } else if(from !== settings.nick ){
-      if(optOuts.indexOf(from) === -1 || force){
+      if(optOuts.indexOf(from) !== -1 || force){
         discordClient.reportMessage(from,discordClient.channelID,message);
+      }
+    }
+    console.log(`<${from}>| ${message}`);
+});
+client.addListener('action', function (from,to,text,message) {
+    if(message === '!optin'){
+      if(optOuts.indexOf(from) === -1){
+        optOuts.push(from);
+        updateOptOuts(optOuts);
+      }
+    } else if(message === '!optout'){
+      optOuts = optOuts.filter(i => i !== from);
+      updateOptOuts(optOuts);
+    } else if(from !== settings.nick ){
+      if(optOuts.indexOf(from) !== -1){
+        discordClient.reportAction(from,discordClient.channelID,text,message);
       }
     }
 });
@@ -50,11 +66,21 @@ client.addListener('error', function(message) {
     console.log('error: ', message);
 });
 
-discordClient.bot.on('message', function(user, userID, channelID, message, event) {
-  let channel = discordClient.channelID;
-  //console.log(`${message.substr(0,5)}:${message.substr(5,message.length)}`);
-  if(userID !== discordClient.bot.id && message && channelID === discordClient.channelID){
-    client.say(settings.ircChannel,`<${user}>: ${message}`);
+discordClient.bot.on('any', function(event) {
+  if(event.t == 'MESSAGE_CREATE' && event.d.channel_id === discordClient.channelID && event.d.author.id !== discordClient.bot.id) {
+    let user = event.d.author.username
+    let message = event.d.content
+    if(event.d.attachments.length != 0) {
+      event.d.attachments.forEach( function(value) {
+        let url = value.url
+        client.say(settings.ircChannel,`<${user}> ${url}`);
+        console.log(`<${user}>: ${url}`);
+      });
+    }
+    if(event.d.content !== '') {
+      client.say(settings.ircChannel,`<${user}> ${message}`);
+      console.log(`<${user}>: ${message}`);
+    }
   }
 });
 
