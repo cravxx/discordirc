4c65aee934717251184f0080526b9352fe042b2c
az4521 <cheeze.sprinkels@gmail.com>
2017-12-22 21:49:01 -0500

    preliminary webhook support

--- discord.js
+++ discord.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.io');
 const settings = require('./settings')
+const request = require('request');
 
 let bot = new Discord.Client({
     token: settings.token,
@@ -29,10 +30,24 @@ bot.on('disconnect',()=>{
 let discordClient = {
   bot,
   reportMessage(from,to,message){
-    bot.sendMessage({
-      to: to,
-      message:`**(${from})** ${message}`
-    });
+    request.post(
+        settings.discordWebhook,
+        { json: { 
+                content: message,
+                username: from,
+                avatar_url: "https://robohash.org/"+from+".png"
+                } 
+        },
+        function (error, response, body) {
+            if (response.statusCode != 204) {
+                console.log(response);
+            }
+        }
+    );
+//    bot.sendMessage({
+//      to: to,
+//      message:`**(${from})** ${message}`
+//    });
   },
   reportAction(from,to,text,message){
     bot.sendMessage({
