1e0f244b469284c81b6282c544b017c87fe2c1c2
az4521 <cheeze.sprinkels@gmail.com>
2017-12-22 22:01:42 -0500

    add new fields to settings

--- settings.js
+++ settings.js
@@ -1,8 +1,10 @@
 let settings = {
   nick: 'hsgDiscord',
-  token: '<your token here>',
+  token: '<bot token here>',
   server: 'irc.rizon.net',
   ircChannel: '#/g/ftp',
-  discordChannel: 'g_ftp-bridge'
+  discordChannel: 'g_ftp-bridge',
+  discordWebhookID: '393708094502207490',
+  discordWebhookToken: '<hook token here>'
 };
 module.exports = settings;
